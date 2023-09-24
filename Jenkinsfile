ee90cfe374f1ef57aa13e403aff3bae10b6618db
EC2 Default User <ec2-user@ip-10-0-2-14.ec2.internal>
2023-09-24 14:27:08 +0000

    updated terrform file and eks module

--- Jenkinsfile
+++ Jenkinsfile
@@ -20,39 +20,39 @@ pipeline {
                 }
             }
         }
-        // stage ("Fetch Code") {
-        //     steps {
-        //         script {
-        //             gv.fetchCode()
-        //         }
-        //     }
-        // }
-        // stage ('Build') {
-        //     steps {
-        //         script {
-        //             gv.buildCode()
-        //         }
-        //     }
-        //     post {
-        //         success {
-        //             archiveArtifacts artifacts: '**/*.war'
-        //         }
-        //     }
-        // }
-        // stage ('Build Image') {
-        //     steps {
-        //         script {
-        //             gv.buildImage()
-        //         }
-        //     }
-        // }
-        // stage ('Push Image to ECR') {
-        //     steps {
-        //         script {
-        //             gv.pushImage()
-        //         }
-        //     }
-        // }
+        stage ("Fetch Code") {
+            steps {
+                script {
+                    gv.fetchCode()
+                }
+            }
+        }
+        stage ('Build') {
+            steps {
+                script {
+                    gv.buildCode()
+                }
+            }
+            post {
+                success {
+                    archiveArtifacts artifacts: '**/*.war'
+                }
+            }
+        }
+        stage ('Build Image') {
+            steps {
+                script {
+                    gv.buildImage()
+                }
+            }
+        }
+        stage ('Push Image to ECR') {
+            steps {
+                script {
+                    gv.pushImage()
+                }
+            }
+        }
         stage ('provision eks cluster') {
             steps {
                 script {
@@ -60,12 +60,12 @@ pipeline {
                 }
             }
         }
-        // stage ('connect to eks cluster') {
-        //     steps {
-        //         script {
-        //             gv.connectEks()
-        //         }
-        //     }
-        // }
+        stage ('connect to eks cluster') {
+            steps {
+                script {
+                    gv.connectEks()
+                }
+            }
+        }
     }
 }
--- script.groovy
+++ script.groovy
@@ -1,25 +1,25 @@
-// def fetchCode() {
-//     echo "Pull Source code from GitHub"
-//     git branch: 'main', url: 'https://github.com/profebass99/jenkins-terraform-eks.git'
-// }
+def fetchCode() {
+    echo "Pull Source code from GitHub"
+    git branch: 'main', url: 'https://github.com/profebass99/jenkins-terraform-eks.git'
+}
 
-// def buildCode() {
-//     echo "Build App with Maven"
-//     sh 'mvn clean install -DskipTests'
-// }
+def buildCode() {
+    echo "Build App with Maven"
+    sh 'mvn clean install -DskipTests'
+}
 
-// def buildImage() {
-//     echo "Build app with docker"
-//     dockerImage = docker.build(awsEcrRegistry + ":$BUILD_NUMBER", "./Docker-files/app/multistage/")
-// }
+def buildImage() {
+    echo "Build app with docker"
+    dockerImage = docker.build(awsEcrRegistry + ":$BUILD_NUMBER", "./Docker-files/app/multistage/")
+}
 
-// def pushImage() {
-//     echo "Push Docker Image to ECR"
-//     docker.withRegistry (imageRegUrl, awsEcrCreds) {
-//         dockerImage.push ("$BUILD_NUMBER")
-//         dockerImage.push ('latest')
-//     }
-// }
+def pushImage() {
+    echo "Push Docker Image to ECR"
+    docker.withRegistry (imageRegUrl, awsEcrCreds) {
+        dockerImage.push ("$BUILD_NUMBER")
+        dockerImage.push ('latest')
+    }
+}
 
 def provisionEksCluster() {
     withAWS(credentials: 'JenkinsAWSCLI', region: 'us-east-2') {
@@ -34,16 +34,16 @@ def provisionEksCluster() {
     }
 }
 
-// def connectEks() {
-//     echo "${EKS_CLUSTER_NAME}"
-//     withAWS(credentials: 'JenkinsAWSCLI', region: 'us-east-2') {
-//         sh "aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${awsRegion}"
-//         sh 'kubectl get nodes'
-//         sh 'kubectl apply -f app.yaml'
-//         sh 'sudo cp /var/lib/jenkins/.kube/config ~/.kube/.'
-//         sh 'sudo chown -R ubuntu:ubuntu config'
-//     }
-// }
+def connectEks() {
+    echo "${EKS_CLUSTER_NAME}"
+    withAWS(credentials: 'JenkinsAWSCLI', region: 'us-east-2') {
+        sh "aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${awsRegion}"
+        sh 'kubectl get nodes'
+        sh 'kubectl apply -f app.yaml'
+        sh 'sudo cp /var/lib/jenkins/.kube/config ~/.kube/.'
+        sh 'sudo chown -R ubuntu:ubuntu config'
+    }
+}
 
 return this 
 
